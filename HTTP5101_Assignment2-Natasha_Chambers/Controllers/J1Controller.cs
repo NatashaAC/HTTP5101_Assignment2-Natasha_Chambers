983dbfba18da54e6780172b338851b29d9ce7cda
Natasha Chambers <natashaagathachambers@gmail.com>
2021-02-24 14:18:41 -0500

    J1 controller now accepts decimal input, added more comments, and changed return message

--- HTTP5101_Assignment2-Natasha_Chambers/Controllers/J1Controller.cs
+++ HTTP5101_Assignment2-Natasha_Chambers/Controllers/J1Controller.cs
@@ -9,7 +9,7 @@ namespace HTTP5101_Assignment2_Natasha_Chambers.Controllers
 {
     public class J1Controller : ApiController
     {
-        /* J1 2008 Problem: Body Mass Index 
+        /* J1 2008 Problem - Body Mass Index 
          * Objective:
          * Calculate the BMI of a patient based on their height in metres and weight in kilograms. 
          * Then displays a message telling the patient that they are overweight, 
@@ -17,38 +17,60 @@ namespace HTTP5101_Assignment2_Natasha_Chambers.Controllers
         */
 
         /// <summary>
-        /// Returns health condition of a patient which is overweight, normal weight, or 
-        /// underweight based on their BMI calculated by their input of  weight in kilograms
-        /// and height in metres
+        ///     Returns health condition of a patient which is overweight, normal weight, or 
+        ///     underweight based on their BMI calculated by their input of weight in kilograms
+        ///     and height in metres
         /// </summary>
-        /// <param name="weight">decimal value of the patient's weight</param>
-        /// <param name="height">decimal value of the patient's height</param>
-        /// <returns> a word related to the BMI of a patient </returns>
-        /// <example> GET api/J1/BMICalculator/69/1.73 -> Normal weight </example>
-        /// <example> GET api/J1/BMICalculator/84.5/1.8 -> Overweight </example>
-        /// <example> GET api/J1/BMICalculator/45/1.65 -> Underweight </example>
-        /// <example> GEt api/J1/BMICalculator/-78/1.40 -> Unable to calculate BMI </example>
-
+        /// <param name="weight"> decimal value of a patient's weight </param>
+        /// <param name="height"> decimal value of a patient's height </param>
+        /// <returns> A sentence describing the BMI of a patient </returns>
+        /// <example> 
+        ///     GET api/J1/BMICalculator/69/1.73/ -> 
+        ///     "The patient's BMI is . According to the table, the patient is Normal weight"
+        /// </example>
+        /// <example> 
+        ///     GET api/J1/BMICalculator/84.5/1.8/ -> 
+        ///     "The patient's BMI is . According to the table, the patient is Overweight"
+        /// </example>
+        /// <example> 
+        ///     GET api/J1/BMICalculator/45/1.65/ -> Underweight 
+        ///     "The patient's BMI is . According to the table, the patient is Underweight"
+        /// </example>
+        /// <example> 
+        ///     GET api/J1/BMICalculator/-78/1.40/ -> "Unable to calculate BMI"
+        /// </example>
         [HttpGet]
-        [Route("api/J1/BMICalculator/{weight}/{height}")]
+        [Route("api/J1/BMICalculator/{weight}/{height}/")]
         public string BMICalculator(decimal weight, decimal height)
         {
+            // BMI formula
             decimal bmi = weight / (height * height);
 
-            string message = "";
+            string category = "";
 
-            if (bmi > 25)
+            // Logic to deal with negative input for height and weight
+            if (weight < 0 || height < 0)
             {
-                message = "Overweight";
-            }
-            else if (bmi > 18 && bmi < 25)
+                return "Unable to calculate BMI";
+            } else
             {
-                message = "Normal weight";
+                // Logic to determine what category the patient is in
+                if (bmi > 25)
+                {
+                    category = "Overweight";
+                }
+                else if (bmi > 18  && bmi < 25)
+                {
+                    category = "Normal weight";
+                }
+                else if (bmi < 18)
+                {
+                    category = "Underweight";
+                }
             }
-            else if (bmi < 18)
-            {
-                message = "Underweight";
-            }
+
+            // Message that stores the bmi calculation and category
+            string message = "The patient's BMI is " + Math.Round(bmi, 2) + ". According to the table, the patient is " + category;
 
             return message;
         }
---
+++ HTTP5101_Assignment2-Natasha_Chambers/Controllers/J3Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HTTP5101_Assignment2_Natasha_Chambers.Controllers
+{
+    public class J3Controller : ApiController
+    {
+        /* J3/S1 2002 Problem - The Students' Council Breakfast
+         * Objective:
+         * Determine the different combinations of tickets and minimum number of 
+         * tickets to print in order to reach the amount needed to be raised. 
+         * The cost of each PINK, GREEN, RED, and ORANGE ticket is based on how 
+         * many years a student has been in school. The amount to raise is based 
+         * on the student council's input. Then display the number of combinations 
+         * and minimum number of tickets to print.
+         */
+    }
+}
